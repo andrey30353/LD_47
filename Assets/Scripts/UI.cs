328d2c4af42a2a2de32606b73d5a5810cebae486
Sasashasa <sasashasa321@gmail.com>
2020-10-03 22:38:47 +0700

    Наброски меню

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
