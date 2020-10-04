3e202aef8ded87374f21caafc96bc5f57275f3b0
Sasashasa <sasashasa321@gmail.com>
2020-10-04 13:01:23 +0700

    UI

---
+++ Assets/Scripts/SoundEffector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffector : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip crawlSound;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayCrawlSound()
+    {
+        audioSource.PlayOneShot(crawlSound);
+    }
+}
