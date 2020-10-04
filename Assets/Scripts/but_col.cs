80d9275a0184f90c6c09f0db1f9dbf55fd628766
Sasashasa <sasashasa321@gmail.com>
2020-10-04 23:25:34 +0700

    Tutorial and eat

---
+++ Assets/Scripts/but_col.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+public class but_col : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+ 
+    private Button but;
+    private ColorBlock old;
+    public ColorBlock neww;
+ 
+    void Start () {
+        but = this.GetComponent<Button>();
+        old = but.colors;
+    }
+ 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        but.colors = neww;
+   }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        but.colors = old; 
+    }
+}
